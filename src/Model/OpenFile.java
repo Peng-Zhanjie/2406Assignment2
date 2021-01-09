1415c99edb8aa9c7a19b43b543102722b5decb88
Peng-Zhanjie <nowarzenx@gmail.com>
2021-01-09 16:49:47 +0800

    Add_OpenFile_Class

---
+++ src/Model/OpenFile.java
@@ -0,0 +1,24 @@
+package Model;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class OpenFile {
+    Path file= Paths.get("SaveFile.txt");
+    public byte[] getItem(){
+        try {
+              InputStream input= new BufferedInputStream(Files.newInputStream(file));
+              byte data[] = new byte[1024] ;
+              int len = input.read(data);
+            System.out.print("Open Finished\n");
+              return data;
+        } catch (Exception e) {
+              System.out.println("Message: " + e);
+              return new byte[1024];
+        }
+
+    }
+
+}
